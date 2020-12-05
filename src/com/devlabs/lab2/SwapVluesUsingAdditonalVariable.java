34aae1b1e7c31e4d95c3880f2f7e5bfebaa9c7e8
SarjanaS <SarjanaS@HBADT4700105.bsg.ad.adp.com>
2020-12-05 16:53:39 +0530

    First commit_01

--- src/com/devlabs/lab2/SwapVluesUsingAdditonalVariable.java
+++ src/com/devlabs/lab2/SwapVluesUsingAdditonalVariable.java
@@ -1,5 +1,18 @@
 package com.devlabs.lab2;
 
-public class SwapVluesUsingAdditonalVariable {
+public class SwapValuesUsingAdditionalVariable {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        float a = 10.31f;
+        float b = 2.69f;
+        float c ;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
 
 }
