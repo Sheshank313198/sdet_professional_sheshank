82af8d03aad200a0764758b6349f5a22d4041d58
SarjanaS <SarjanaS@HBADT4700105.bsg.ad.adp.com>
2021-04-09 19:59:48 +0530

    Java Assignments

--- src/com/devlabs/lab5/CheckPrimeFor.java
+++ src/com/devlabs/lab5/CheckPrimeFor.java
@@ -30,27 +30,4 @@ public class CheckPrimeFor {
       else
           System.out.println(num + " is a prime no");
   }
-      Scanner scan = new Scanner(System.in);
-      System.out.println("Please input the number: ");
-      int num = scan.nextInt();
-      
-      scan.close();
-      boolean flag = false;
-      
-      for(int i=2;i<=num/2;i++)
-      {
-          if(num%i == 0)
-          { 
-              flag = true;
-              System.out.println(flag);
-          }
       }
-      
-      
-      if(flag)
-        System.out.println(num + " is not a prime no");
-      else
-          System.out.println(num + " is a prime no");
-  }
-
-}
